bb6c1f1e39fcdbddd04dd40ec41956585e48988e
fady <fadym11111@gmail.com>
2026-09-05 21:18:39 +0300

    Runnable Paralle

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import (ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate)
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel
-
+import os
 from pydantic import BaseModel
 
 load_dotenv()
 app = FastAPI()
-
+print("GROQ KEY EXISTS:", bool(os.getenv("GROQ_API_KEY")))
 class ContentRequest(BaseModel):
     content:str
 
@@ -33,10 +33,9 @@ Result
 @app.post('/analysis')
 def analysis(request:ContentRequest):
     # define moedl
-    model = ChatGroq(model='openai/gpt-oss-20b')
+    model = ChatGroq(model='openai/gpt-oss-20b',api_key=os.getenv("GROQ_API_KEY"))
 
-    # define prompt
-
+    # define prompt all task
     prompt_templete = ChatPromptTemplate.from_messages([
        SystemMessagePromptTemplate.from_template("""
   You are an AI content analyst.
@@ -51,8 +50,54 @@ def analysis(request:ContentRequest):
     HumanMessagePromptTemplate.from_template('{content}')
    ])
 
+    # summary
+    prompt_templete_summary = ChatPromptTemplate.from_messages([
+       SystemMessagePromptTemplate.from_template("""
+        You are an AI content summarization assistant.
+        Summarize the provided content in 2-3 concise sentences.
+        Do not add information that is not present in the content.
+        """),
+    HumanMessagePromptTemplate.from_template('{content}')
+   ])
+
+    # sentiment
+    prompt_templete_sentiment = ChatPromptTemplate.from_messages([
+       SystemMessagePromptTemplate.from_template("""
+        You are a sentiment analysis assistant.
+        Analyze the sentiment of the provided content.
+        Return one of:
+        Positive
+        Negative
+        Neutral
+        Then briefly explain why.
+        """),
+    HumanMessagePromptTemplate.from_template('{content}')
+   ])
+
+    # topics
+    prompt_templete_topics = ChatPromptTemplate.from_messages([
+       SystemMessagePromptTemplate.from_template("""
+        You are a topic extraction assistant.
+        Extract the main topics discussed in the content.
+        Return them as a short bullet list.
+        """),
+    HumanMessagePromptTemplate.from_template('{content}')
+   ])
+
     # define parser
     parser = StrOutputParser()
-    content_analysis_chain = prompt_templete | model | parser
+
+    # pipline multi models
+    chain_summary = prompt_templete_summary | model | parser
+    chain_sentiment = prompt_templete_sentiment | model | parser
+    chain_topics = prompt_templete_topics | model | parser
+
+    # paralle runnable
+
+    content_analysis_chain =RunnableParallel(
+        sentiment=chain_sentiment,
+        summary=chain_summary,
+        topics=chain_topics
+        )
     result_con = content_analysis_chain.invoke({"content": request.content})
     return {'data':result_con}
